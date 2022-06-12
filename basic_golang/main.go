32aa5573a5357110441d4b110c2df327a9a8f2b7
talkanda <talkanda@gmail.com>
2022-06-12 14:54:18 +0700

    basic syntax

--- basic_golang/main.go
+++ basic_golang/main.go
@@ -1,17 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
-
-	//for mirip foreach
-	title := "Golang"
-	for index, char := range title {
-		fmt.Println("Index ke : ", index, " hurufnya : ", string(char))
-	}
+	// LoopingFor()
+	// TestArray()
+	// ContohSlice()
+	// MyMap()
+	// ContohSliceOfMap()
+	Scores()
 
 }
